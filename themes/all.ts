b1d29023e9ac940d757e29c0c9563b2f0d7e2e3c
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2024-08-10 14:19:12 -0400

    Update all.ts

--- themes/all.ts
+++ themes/all.ts
@@ -1,12 +1,12 @@
 import teal from "./list/teal";
-import blue from "./list/blue";
+import vidbinge from "./list/blue";
 import red from "./list/red";
 import gray from "./list/gray";
 import blck from "./list/blck";
 
 export const allThemes = [
   teal,
-  blue,
+  vidbinge,
   gray,
   red,
   blck
