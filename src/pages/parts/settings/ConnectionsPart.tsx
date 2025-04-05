9f804efa6305d92eb647a7fda876019cfcc6376d
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-04-05 17:23:53 -0600

    remove slash on the end 😭

--- src/pages/parts/settings/ConnectionsPart.tsx
+++ src/pages/parts/settings/ConnectionsPart.tsx
@@ -29,10 +29,11 @@ function ProxyEdit({ proxyUrls, setProxyUrls }: ProxyEditProps) {
 
   const changeItem = useCallback(
     (index: number, val: string) => {
+      const cleanedUrl = val.replace(/\/+$/, "");
       setProxyUrls((s) => [
         ...(s ?? []).map((v, i) => {
           if (i !== index) return v;
-          return val;
+          return cleanedUrl;
         }),
       ]);
     },
