41e7206796fac9ea4452d425ec1507b240105282
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2025-06-09 16:57:40 -0400

    Update LinksDropdown.tsx

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -284,18 +284,6 @@ export function LinksDropdown(props: { children: React.ReactNode }) {
               {t("navigation.menu.logout")}
             </DropdownLink>
           ) : null}
-          <Divider />
-          <div className="my-4 flex justify-center items-center gap-4">
-            <CircleDropdownLink
-              href={conf().DISCORD_LINK}
-              icon={Icons.DISCORD}
-            />
-            <CircleDropdownLink href="/support" icon={Icons.SUPPORT} />
-            <CircleDropdownLink
-              href="https://rentry.co/h5mypdfs"
-              icon={Icons.TIP_JAR}
-            />
-          </div>
         </div>
       </Transition>
     </div>
