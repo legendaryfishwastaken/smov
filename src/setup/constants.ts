1338a9a0610c532c15b30e9bfb3127a28d3f93ff
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2025-06-09 17:30:52 -0400

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.gg/7z6znYgrTG";
-export const GITHUB_LINK = "https://github.com/p-stream/p-strean";
-export const TWITTER_LINK = "https://twitter.com/SudoFlix";
+export const DISCORD_LINK = "https://discord.com/vanityurl/dotcom/steakpants/flour/flower/index11.html";
+export const GITHUB_LINK = "https://github.com/legendaryfishwastaken/smov";
+export const TWITTER_LINK = "https://x.com";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // Do it clap??
